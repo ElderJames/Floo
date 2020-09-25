d761e33bfdc01309862ab427060831952b8f349b
Zhenyu,Liu <liuzhenyulive@live.com>
2020-09-26 00:46:10 +0800

    use row replace space and update style

--- src/Floo.App.Web/Pages/Article.razor.cs
+++ src/Floo.App.Web/Pages/Article.razor.cs
@@ -41,7 +41,7 @@ A community site.
 "
 
             };
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 5; i++)
             {
                 _Comments.Add(new CommentDto
                 {
